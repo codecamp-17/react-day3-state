994f9ed4fa3a33ff139555fbcb377a97dea61515
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-29 16:53:35 +0700

    finish:lab-1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// ## Lab1
 function App() {
-  return <div>Hello React App</div>;
+  const handleChange = (event) => console.log(event.target.value);
+  return <input onChange={handleChange} />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
