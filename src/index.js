cb6f868d51c2bb72e7e57325555a3cb3519b599b
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-30 10:33:58 +0700

    finish:ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+// import 3rd Party
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// import CSS to JS (Global CSS)
+import './index.css';
+
 function App() {
-  return <div>Hello React App</div>;
+  return (
+    <div className='app'>
+      <form className='form'>
+        <h2>Register Form</h2>
+        <input />
+        <input />
+        <input />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
