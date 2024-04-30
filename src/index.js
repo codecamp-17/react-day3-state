6138b6c66416add99ac24e8f431223d19c651974
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-30 15:32:49 +0700

    set-state by callback

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
-  return <div>Hello React App</div>;
+  const [count, setCount] = useState(0);
+
+  const increaseByOne = () => setCount(count + 1);
+  const increaseByThree = () => {
+    // setCount(count + 3);
+    // setCount(count + 1); // count + 1 == 0 + 1
+    // setCount(count + 1); // count + 1 == 0 + 1
+    // setCount(count + 1); // count + 1 == 0 + 1
+
+    // Callback : oldState => neState
+    // setCount((prev) => prev + 3);
+    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1);
+  };
+  return (
+    <div>
+      <button onClick={increaseByOne}>increase</button>
+      <button onClick={increaseByThree}>increase by 3</button>
+      <h2>count : {count} </h2>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
