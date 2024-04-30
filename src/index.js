d7e7bd42860ed0c482b1e04279c91e7f4ce1d877
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-30 09:57:59 +0700

    finish:lab1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Event Handler : Step
+// - UI
+// - UI มี Interaction ? => Handler Fn
+// - Binding Fn
+
+// State
+// - ใช้ State จัดการอะไร (eg. ข้อมูลแสดงผล, เปลี่ยนการแสดงผล ✅)
+// - hardCode State ด้วย JS-Variable => Test Toggle ✅
+// - ใช้ State (state ตั้งตั้น) => Binding Value กับ UI
+
 function App() {
-  return <div>Hello React App</div>;
+  const [isShow, setIsShow] = React.useState(true);
+
+  const handleClick = (event) => {
+    console.log('Click');
+    setIsShow(false);
+  };
+
+  // const isShow = false;
+
+  return <>{isShow ? <button onClick={handleClick}>Click to hide me</button> : null}</>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
