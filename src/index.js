94edc8c6b82459d84cb8333b68f98b2f5b97da1b
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-30 16:16:35 +0700

    separate:component

--- src/index.js
+++ src/index.js
@@ -2,27 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function AddTodo() {
+  return (
+    <header className='todo__add'>
+      <input />
+      <button>add</button>
+    </header>
+  );
+}
+function TodoList() {
+  return (
+    <ul className='todo__list'>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </ul>
+  );
+}
+function TodoItem() {
+  return (
+    <li className='todo__item'>
+      <p>item-1</p>
+      <button>edit</button>
+      <button>x</button>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div className='app'>
       <div class='todo'>
-        <header className='todo__add'>
-          <h1>TodoList</h1>
-          <input />
-          <button>add</button>
-        </header>
-        <ul className='todo__list'>
-          <li className='todo__item'>
-            <p>item-1</p>
-            <button>edit</button>
-            <button>x</button>
-          </li>
-          <li className='todo__item'>
-            <p>item-2</p>
-            <button>edit</button>
-            <button>x</button>
-          </li>
-        </ul>
+        <h1>TodoList</h1>
+        <AddTodo />
+        <TodoList />
       </div>
     </div>
   );
