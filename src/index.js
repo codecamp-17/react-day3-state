1a78fdec4a69c2d86cbf797aa47a4f4bc6f30d29
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-30 09:22:35 +0700

    finish:lab2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// ## Lab1
+// ## Lab2
+// - UI
+// - Handler Fn
+// - UI + Handler FN (Binding Event)
 function App() {
-  const handleChange = (event) => console.log(event.target.value);
-  return <input onChange={handleChange} />;
+  const handleSelect = (event) => {
+    console.log(event.target.value);
+  };
+  return (
+    <select onChange={handleSelect}>
+      <option value='THAILAND'>Thailand</option>
+      <option value='LAOS'>Laos</option>
+      <option value='MYANMAR'>Myanmar</option>
+      <option value='VIETNAM'>VietNam</option>
+      <option value='MALAYSIA'>Malaysia</option>
+    </select>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
