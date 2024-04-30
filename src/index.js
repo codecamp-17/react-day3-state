45bab7c9335880fe94d98422a24f893b8a0b84cf
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-30 13:33:29 +0700

    finish:text-validation

--- src/index.js
+++ src/index.js
@@ -17,17 +17,22 @@ import './index.css';
 //     username : required, length 8 - 24
 //     password : required, length 8 - 16;
 //     confirmPassword : required,  matching, length 8 - 16,
+//  ฝึกทำ State Management 15-20 นาที
+//  - validation Text
+//  - border
 function App() {
   // State
   const [username, setUsername] = React.useState('');
   const [errorUserName, setErrorUsername] = React.useState(false);
-  const [errorUserNameText, setErrorUserNameText] = React.useState('username is required !!');
+  const [errorUserNameText, setErrorUserNameText] = React.useState('');
 
   const [password, setPassword] = React.useState('');
-  const [errorPassword, setErrorPassword] = React.useState(true);
+  const [errorPassword, setErrorPassword] = React.useState(false);
+  const [errorPasswordText, setErrorPasswordText] = React.useState('');
 
   const [confirmPassword, setConfirmPassword] = React.useState('');
   const [errorConfirmPassword, setErrorConfirmPassword] = React.useState(true);
+  const [errorConfirmPasswordText, setErrorConfirmPasswordText] = React.useState('');
   // state สำหรับ password,confirmPassword
 
   // handler FN
@@ -40,11 +45,17 @@ function App() {
   };
   const handleChangePassword = (event) => {
     setPassword(event.target.value);
-    if (errorPassword) setErrorPassword(false);
+    if (errorPassword) {
+      setErrorPassword(false);
+      setErrorPasswordText('');
+    }
   };
   const handleChangeConfirmPassword = (event) => {
     setConfirmPassword(event.target.value);
-    if (errorConfirmPassword) setErrorConfirmPassword(false);
+    if (errorConfirmPassword) {
+      setErrorConfirmPassword(false);
+      setErrorConfirmPasswordText('');
+    }
   };
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -59,32 +70,59 @@ function App() {
       setErrorUserNameText('username must have  8-24 character');
     }
 
-    if (!password) setErrorPassword(true);
-    if (password !== confirmPassword) setErrorConfirmPassword(true);
+    if (!password) {
+      setErrorPassword(true);
+      setErrorPasswordText('password is required');
+    } else if (password.length < 8 || password.length > 16) {
+      setErrorPassword(true);
+      setErrorPasswordText('password must have 8-16 character');
+    }
+    if (!confirmPassword) {
+      setErrorConfirmPassword(true);
+      setErrorConfirmPasswordText('confirm password is required');
+    } else if (confirmPassword.length < 8 || confirmPassword.length > 16) {
+      setErrorConfirmPassword(true);
+      setErrorConfirmPasswordText('confirm password must have 8-16 character');
+    } else if (password !== confirmPassword) {
+      setErrorConfirmPassword(true);
+      setErrorConfirmPasswordText('confirm password mismatch');
+    }
+
+    // Save Backend
+    // if any Error => Early Return
+    // if not Error => Save Backend
+
+    // ResetForm => ทำให้ State ทุกตัวกลับไปที่ค่าเดิม
   };
   return (
     <div className='app'>
       <form className='form' onSubmit={handleSubmit}>
         <h2>Register Form</h2>
         <div className='form__input'>
-          <label>username </label>
+          <label>
+            username<span class='required__text'>*</span>
+          </label>
           <input className='input__error' value={username} onChange={handleChangeUserName} />
           {errorUserName && <p className='text__error'>{errorUserNameText}</p>}
         </div>
         <div className='form__input'>
-          <label>password </label>
+          <label>
+            password<span class='required__text'>*</span>
+          </label>
           <input
             type='password'
             className='input__success'
             value={password}
             onChange={handleChangePassword}
           />
-          {errorPassword && <p className='text__error'>password is required</p>}
+          {errorPassword && <p className='text__error'>{errorPasswordText}</p>}
         </div>
         <div className='form__input'>
-          <label>confirm password </label>
+          <label>
+            confirm password<span class='required__text'>*</span>
+          </label>
           <input type='password' value={confirmPassword} onChange={handleChangeConfirmPassword} />
-          {errorConfirmPassword && <p className='text__error'>confirm password mismatch</p>}
+          {errorConfirmPassword && <p className='text__error'>{errorConfirmPasswordText}</p>}
         </div>
         <button type='submit'>Sign Up</button>
       </form>
