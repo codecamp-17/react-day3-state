983ffded73b9888c3a438596550102b7ead3346a
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-30 16:08:00 +0700

    css:finish ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 function App() {
-  return <div>Hello React App</div>;
+  return (
+    <div className='app'>
+      <div class='todo'>
+        <header className='todo__add'>
+          <h1>TodoList</h1>
+          <input />
+          <button>add</button>
+        </header>
+        <ul className='todo__list'>
+          <li className='todo__item'>
+            <p>item-1</p>
+            <button>edit</button>
+            <button>x</button>
+          </li>
+          <li className='todo__item'>
+            <p>item-2</p>
+            <button>edit</button>
+            <button>x</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
