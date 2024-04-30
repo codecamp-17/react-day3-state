5b37e82e4bd1d6c4cac5084769315d209364c03e
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-30 09:46:23 +0700

    finish:lab4

--- src/index.js
+++ src/index.js
@@ -1,39 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// ## Lab3
+// ## Lab4
 // - UI
 // - Handler Function
 // - Binding Event (UI + Handler FN)
 function App() {
-  const handleCheck = (event) => {
-    console.log(event.target.name);
-    console.log(event.target.value);
-    console.log(event.target.checked);
+  const handleRedirect = (event) => {
+    const ans = window.confirm('Redirect to google ?');
+    if (ans === true) {
+      // window.location.href = 'google.com';
+      window.location.replace('https://google.com');
+    }
   };
-  return (
-    <>
-      <input
-        type='checkbox'
-        name='phoneBrand'
-        id='phone-1'
-        value='SAMSUNG'
-        onChange={handleCheck}
-      />
-      <label htmlFor='phone-1'>Samsung</label>
-      <br />
-      <input
-        type='checkbox'
-        name='phoneBrand'
-        id='phone-2'
-        value='IPHOONE'
-        onChange={handleCheck}
-      />
-      <label htmlFor='phone-2'>IPhoone</label> <br />
-      <input type='checkbox' name='phoneBrand' id='phone-3' value='HUAWI' onChange={handleCheck} />
-      <label htmlFor='phone-3'>HuaWi</label> <br />
-    </>
-  );
+  return <a onClick={handleRedirect}>Google</a>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
