0e09af479a32cff591e99dd9550dca3768a02acb
pavitpim40_mbp <pavitpim@gmail.com>
2024-04-30 11:15:58 +0700

    feat:binding event

--- src/index.js
+++ src/index.js
@@ -5,27 +5,40 @@ import ReactDOM from 'react-dom/client';
 // import CSS to JS (Global CSS)
 import './index.css';
 
+// #1 UI ✅
+// #2 Event : Handler -> 4 ที่
+// - handler ✅
+// - binding ✅
+
 function App() {
+  // handler FN
+  const handleChangeUserName = (event) => console.log(event.target.value);
+  const handleChangePassword = (event) => console.log(event.target.value);
+  const handleChangeConfirmPassword = (event) => console.log(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submit');
+  };
   return (
     <div className='app'>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h2>Register Form</h2>
         <div className='form__input'>
           <label>username </label>
-          <input className='input__error' />
+          <input className='input__error' onChange={handleChangeUserName} />
           <p className='text__error'>username is required</p>
         </div>
         <div className='form__input'>
           <label>password </label>
-          <input className='input__success' />
+          <input className='input__success' onChange={handleChangePassword} />
           <p className='text__error'>password is required</p>
         </div>
         <div className='form__input'>
-          <label>confirmPassword </label>
-          <input />
+          <label>confirm password </label>
+          <input onChange={handleChangeConfirmPassword} />
           <p className='text__error'>confirm password mismatch</p>
         </div>
-        <button>Submit</button>
+        <button type='submit'>Sign Up</button>
       </form>
     </div>
   );
