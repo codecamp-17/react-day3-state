3767706ad732149c3170e3e15e539eb831d72d1e
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-01 16:49:15 +0700

    finish:css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 function App() {
-  return <div>Hello React App</div>;
+  return (
+    <div className='app'>
+      <button>open</button>
+      <div className='modal'>
+        <div className='modal__content'>
+          <h3>Are you ok ?</h3>
+          <p>just checking in</p>
+          <button className='modal__btn--confirm'>Yes Thanks</button>
+          <button className='modal__btn--close'>x</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
