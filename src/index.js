d2aab61c0e6a0f092eef20788085804214067d94
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-01 15:51:05 +0700

    refactor:separate file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-function Accordion() {
-  const [isShow, setIsShow] = useState(false);
-
-  const handleToggle = () => {
-    setIsShow((p) => !p);
-  };
-  return (
-    <div className='accordion'>
-      <div className='accordion__title'>
-        <h3>section title</h3>
-        <button onClick={handleToggle}>toggle</button>
-      </div>
-      {isShow && (
-        <div className='accordion__detail'>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur odit id totam vitae at
-            vero? Repellendus maxime animi eos eaque porro, in doloribus veritatis dolorum quasi
-            voluptatibus. Error, similique non. Quaerat voluptas ad fugiat quisquam ducimus
-            doloremque incidunt distinctio quas, libero ut iusto illum fugit tenetur voluptates
-            soluta ipsam perferendis.
-          </p>
-        </div>
-      )}
-    </div>
-  );
-}
+import Accordion from './components/Accordion';
 
 function App() {
   return (
